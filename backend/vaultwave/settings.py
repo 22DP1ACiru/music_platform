75fe7f177e925c1413355dbd7f3f6970c5f59d22
Aigars Cīrulis <cirulis.aigars@outlook.com>
2025-04-09 02:33:32 +0300

    Add API views and routes to backend

--- backend/vaultwave/settings.py
+++ backend/vaultwave/settings.py
@@ -119,6 +119,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         # Configure auth methods later
+         'rest_framework.authentication.SessionAuthentication',
          'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
--- backend/vaultwave/urls.py
+++ backend/vaultwave/urls.py
@@ -1,22 +1,38 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+from users.views import UserViewSet, UserProfileViewSet
+from music.views import (
+    GenreViewSet, ArtistViewSet, ReleaseViewSet,
+    TrackViewSet, CommentViewSet, HighlightViewSet
+)
+from playlists.views import PlaylistViewSet
+
+router = DefaultRouter()
+
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'profiles', UserProfileViewSet, basename='userprofile')
+
+router.register(r'genres', GenreViewSet)
+router.register(r'artists', ArtistViewSet)
+router.register(r'releases', ReleaseViewSet, basename='release')
+router.register(r'tracks', TrackViewSet)
+router.register(r'comments', CommentViewSet)
+router.register(r'highlights', HighlightViewSet)
+
+router.register(r'playlists', PlaylistViewSet, basename='playlist')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/', include(router.urls)),
+
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
